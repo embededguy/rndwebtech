0d2913322cfd92b00d3d61a5755d156348bddfa0
embededguy <prakash.choudhary.extc@gmail.com>
2025-04-11 10:19:49 +0530

    final updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  distDir: "build",
-
-
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
